e2ee652ee22e81e6a98d5eed185eba75ef8702c9
kap <kgdee7603@gmail.com>
2023-05-17 05:08:20 +0800

    commit

--- script.js
+++ script.js
@@ -1,832 +1,177 @@
-const audios = ["Ab","Bbm","Cm","Db","Eb","Eb7","Fm","E","A","F#m","Eb-G","E-G#","D","B","C#","C#m","C#m7","D#","D#m","F#","G#","G#m","G#m7","F","Fm7","Bbm7","Gbm7","Abm"]
+const notes = ["C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3","C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4","C5","C#5","D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5"]
 
-const songs = [
+const chords = [
   {
-    name: "Rubia",
-    src: "https://www.youtube.com/embed/wKVJi-FLvak",
-    waves: [
-      {
-        chord: "Fm",
-        p: "Life blooms"
-      },
-      {
-        chord: "Db",
-        p: "like a flower"
-      },
-      {
-        chord: "Eb",
-        p: "far away or"
-      },
-      {
-        chord: "Ab",
-        p: "by the road"
-      },
-      {
-        chord: "Fm",
-        p: "waiting for"
-      },
-      {
-        chord: "Db",
-        p: "the one to"
-      },
-      {
-        chord: "Eb",
-        p: "find the way back"
-      },
-      {
-        chord: "Ab",
-        p: "home"
-      },
-      {
-        chord: "Fm",
-        p: "Rain falls a"
-      },
-      {
-        chord: "Db",
-        p: "thousand times"
-      },
-      {
-        chord: "Eb",
-        p: "no footprints of"
-      },
-      {
-        chord: "Ab",
-        p: "come-and-go"
-      },
-      {
-        chord: "Fm",
-        p: "You who"
-      },
-      {
-        chord: "Db",
-        p: "once went by"
-      },
-      {
-        chord: "Eb",
-        p: "where will you"
-      },
-      {
-        chord: "Ab",
-        p: "belong? I feel your"
-      },
-      {
-        chord: "Db",
-        p: "sigh and breath in the last"
-      },
-      {
-        chord: "Fm",
-        p: "blow of wind not"
-      },
-      {
-        chord: "Bbm",
-        p: "yet for the story"
-      },
-      {
-        chord: "Cm",
-        p: "on the last page"
-      },
-      {
-        chord: "Db",
-        p: "it's not the"
-      },
-      {
-        chord: "Eb",
-        p: "end"
-      },
-      {
-        chord: "Fm",
-        p: "Life blooms"
-      },
-      {
-        chord: "Db",
-        p: "like a flower"
-      },
-      {
-        chord: "Eb",
-        p: "far away or"
-      },
-      {
-        chord: "Ab",
-        p: "by the road"
-      },
-      {
-        chord: "Eb/G",
-        p: ""
-      },
-      {
-        chord: "Fm",
-        p: "waiting for"
-      },
-      {
-        chord: "Db",
-        p: "the one to"
-      },
-      {
-        chord: "Eb",
-        p: "find the way back"
-      },
-      {
-        chord: "Ab",
-        p: "home"
-      },
-      {
-        chord: "Eb/G",
-        p: ""
-      },
-      {
-        chord: "Fm",
-        p: "time flows"
-      },
-      {
-        chord: "Db",
-        p: "across the world"
-      },
-      {
-        chord: "Db",
-        p: "there is always a"
-      },
-      {
-        chord: "Ab",
-        p: "longer"
-      },
-      {
-        chord: "Eb/G",
-        p: "way to"
-      },
-      {
-        chord: "Fm",
-        p: "go 'Till I"
-      },
-      {
-        chord: "Db",
-        p: "reach your arms A"
-      },
-      {
-        chord: "Eb",
-        p: "madder there for"
-      },
-      {
-        chord: "Ab",
-        p: "you"
-      },
-      {
-        chord: "Fm",
-        p: "Up against the"
-      },
-      {
-        chord: "Db",
-        p: "stream"
-      },
-      {
-        chord: "Eb",
-        p: "waterways will"
-      },
-      {
-        chord: "Ab",
-        p: "join as one"
-      },
-      {
-        chord: "Eb/G",
-        p: ""
-      },
-      {
-        chord: "Fm",
-        p: "tracing"
-      },
-      {
-        chord: "Db",
-        p: "to the source"
-      },
-      {
-        chord: "Eb",
-        p: "no more strayed or"
-      },
-      {
-        chord: "Ab",
-        p: "lost You"
-      },
-      {
-        chord: "Eb/G",
-        p: "will see"
-      },
-      {
-        chord: "Db",
-        p: "petals fly when lament"
-      },
-      {
-        chord: "Fm",
-        p: "becomes carol could"
-      },
-      {
-        chord: "Bbm",
-        p: "you please hear my"
-      },
-      {
-        chord: "Cm",
-        p: "voice that hungers"
-      },
-      {
-        chord: "Db",
-        p: "for a"
-      },
-      {
-        chord: "Eb",
-        p: "duo?"
-      },
-      {
-        chord: "Fm",
-        p: "Life blooms"
-      },
-      {
-        chord: "Db",
-        p: "like a flower"
-      },
-      {
-        chord: "Eb",
-        p: "far away or by"
-      },
-      {
-        chord: "Ab",
-        p: "the road"
-      },
-      {
-        chord: "Eb/G",
-        p: ""
-      },
-      {
-        chord: "Fm",
-        p: "waiting for"
-      },
-      {
-        chord: "Db",
-        p: "the one to"
-      },
-      {
-        chord: "Eb",
-        p: "find the way back"
-      },
-      {
-        chord: "Ab",
-        p: "home"
-      },
-      {
-        chord: "Eb/G",
-        p: ""
-      },
-      {
-        chord: "Fm",
-        p: "time flows"
-      },
-      {
-        chord: "Db",
-        p: "across the world"
-      },
-      {
-        chord: "Eb",
-        p: "there is always a"
-      },
-      {
-        chord: "Ab",
-        p: "longer"
-      },
-      {
-        chord: "Eb/G",
-        p: "way to"
-      },
-      {
-        chord: "F#m",
-        p: "go 'Till I"
-      },
-      {
-        chord: "Db",
-        p: "reach your arms A"
-      },
-      {
-        chord: "Eb",
-        p: "madder there for"
-      },
-      {
-        chord: "Ab",
-        p: "you"
-      },
-      {
-        chord: "Ab",
-        p: "you"
-      },
-      {
-        chord: "Ab",
-        p: "you"
-      },
-      {
-        chord: "F#m",
-        p: "Life blooms"
-      },
-      {
-        chord: "D",
-        p: "like a flower"
-      },
-      {
-        chord: "E",
-        p: "far away or"
-      },
-      {
-        chord: "A",
-        p: "by the road"
-      },
-      {
-        chord: "E/G#",
-        p: ""
-      },
-      {
-        chord: "F#m",
-        p: "waiting for"
-      },
-      {
-        chord: "D",
-        p: "the one to"
-      },
-      {
-        chord: "E",
-        p: "find the way back"
-      },
-      {
-        chord: "A",
-        p: "home"
-      },
-      {
-        chord: "E/G#",
-        p: ""
-      },
-      {
-        chord: "F#m",
-        p: "time flows"
-      },
-      {
-        chord: "D",
-        p: "across the world"
-      },
-      {
-        chord: "E",
-        p: "there is always a"
-      },
-      {
-        chord: "A",
-        p: "longer"
-      },
-      {
-        chord: "E/G#",
-        p: "way to"
-      },
-      {
-        chord: "F#m",
-        p: "go 'Till I"
-      },
-      {
-        chord: "D",
-        p: "reach your arms A"
-      },
-      {
-        chord: "E",
-        p: "madder there for"
-      },
-      {
-        chord: "A",
-        p: "you"
-      },
-      {
-        chord: "D",
-        p: ""
-      },
-      {
-        chord: "E",
-        p: ""
-      },
-      {
-        chord: "A",
-        p: ""
-      },
-    ]
+    key: "Ab",
+    notes: ["G#3","C4","D#4"]
   },
   {
-    name: "The Way of The Decision",
-    src: "https://www.youtube.com/embed/-S5vANztLBM",
-    waves: [
-      {
-        chord: "G#m",
-        p: "-"
-      },
-      {
-        chord: "F#",
-        p: "-"
-      },
-      {
-        chord: "B",
-        p: "-"
-      },
-      {
-        chord: "E",
-        p: "-"
-      },
-      {
-        chord: "F#",
-        p: "-"
-      },
-      {
-        chord: "G#m",
-        p: "-"
-      },
-      {
-        chord: "F#",
-        p: "-"
-      },
-      {
-        chord: "B",
-        p: "-"
-      },
-      {
-        chord: "E",
-        p: "-"
-      },
-      {
-        chord: "G#m",
-        p: "I just want to reach to the"
-      },
-      {
-        chord: "F#",
-        p: "highest"
-      },
-      {
-        chord: "B",
-        p: "of my"
-      },
-      {
-        chord: "E",
-        p: "self"
-      },
-      {
-        chord: "F#",
-        p: "-"
-      },
-      {
-        chord: "G#m",
-        p: "but i too many thing"
-      },
-      {
-        chord: "F#",
-        p: "on the way"
-      },
-      {
-        chord: "B",
-        p: "now i must"
-      },
-      {
-        chord: "E",
-        p: "decide"
-      },
-      {
-        chord: "D#",
-        p: "sometimes i"
-      },
-      {
-        chord: "C#m",
-        p: "wonder about my goal even thought"
-      },
-      {
-        chord: "E",
-        p: "there is another reason i"
-      },
-      {
-        chord: "G#m",
-        p: "leave this story"
-      },
-      {
-        chord: "G#",
-        p: "..."
-      },
-      {
-        chord: "D#m",
-        p: "..."
-      },
-      {
-        chord: "C#",
-        p: "just tell me again what to do now cause i just"
-      },
-      {
-        chord: "E",
-        p: "found that i never been alone"
-      },
-      {
-        chord: "F#",
-        p: "We are the"
-      },
-      {
-        chord: "G#m7",
-        p: "pioneer standing"
-      },
-      {
-        chord: "E",
-        p: "before the sins but the"
-      },
-      {
-        chord: "B",
-        p: "light are shining beyond"
-      },
-      {
-        chord: "F#",
-        p: "..."
-      },
-      {
-        chord: "G#m7",
-        p: "A path appear brightening this"
-      },
-      {
-        chord: "E",
-        p: "tragic but beautiful"
-      },
-      {
-        chord: "B",
-        p: "world... what is right cannot"
-      },
-      {
-        chord: "F#",
-        p: "be foreordained"
-      },
-      {
-        chord: "G#m",
-        p: "..."
-      },
-      {
-        chord: "F#",
-        p: "..."
-      },
-      {
-        chord: "B",
-        p: "..."
-      },
-      {
-        chord: "E",
-        p: "..."
-      },
-      {
-        chord: "G#m",
-        p: "I have to decide my way on some"
-      },
-      {
-        chord: "F#",
-        p: "day even"
-      },
-      {
-        chord: "B",
-        p: "if i dont"
-      },
-      {
-        chord: "E",
-        p: "want to...keep the faith or leave"
-      },
-      {
-        chord: "F#",
-        p: "it"
-      },
-      {
-        chord: "G#m",
-        p: "i dont want to sacrifice any"
-      },
-      {
-        chord: "F#",
-        p: "thing that"
-      },
-      {
-        chord: "B",
-        p: "stand by"
-      },
-      {
-        chord: "E",
-        p: "my side"
-      },
-      {
-        chord: "B",
-        p: "but it used to"
-      },
-      {
-        chord: "C#m7",
-        p: "wonder about my goal even though"
-      },
-      {
-        chord: "E",
-        p: "there's another reason i"
-      },
-      {
-        chord: "G#m",
-        p: "leave this story"
-      },
-      {
-        chord: "B",
-        p: "..."
-      },
-      {
-        chord: "D#m",
-        p: "...just tell"
-      },
-      {
-        chord: "C#m",
-        p: "me again what to"
-      },
-      {
-        chord: "C#",
-        p: "do now... cause i just found"
-      },
-      {
-        chord: "E",
-        p: "that i never been alone...we are the"
-      },
-      {
-        chord: "G#m7",
-        p: "pioneer...standing before"
-      },
-      {
-        chord: "E",
-        p: "the sins"
-      },
-      {
-        chord: "B",
-        p: "but the light are shining"
-      },
-      {
-        chord: "F#",
-        p: "beyond"
-      },
-      {
-        chord: "G#m7",
-        p: "a path appear brightening this"
-      },
-      {
-        chord: "E",
-        p: "tragic but beautiful"
-      },
-      {
-        chord: "B",
-        p: "world without any reason"
-      },
-      {
-        chord: "F#",
-        p: "our light are fading far away now"
-      },
-      {
-        chord: "G#m7",
-        p: "..."
-      },
-      {
-        chord: "E",
-        p: "..."
-      },
-      {
-        chord: "B",
-        p: "..."
-      },
-      {
-        chord: "F#",
-        p: "...whisper"
-      },
-      {
-        chord: "C#",
-        p: "in my eyes..."
-      },
-      {
-        chord: "G#m",
-        p: "Won't let my self regret the"
-      },
-      {
-        chord: "F#",
-        p: "any"
-      },
-      {
-        chord: "B",
-        p: "more"
-      },
-      {
-        chord: "E",
-        p: "anymore"
-      },
-      {
-        chord: "F#",
-        p: "..."
-      },
-      {
-        chord: "G#m7",
-        p: "I am going on wrong way"
-      },
-      {
-        chord: "F#",
-        p: "but i"
-      },
-      {
-        chord: "B",
-        p: "dont"
-      },
-      {
-        chord: "E",
-        p: "care...on"
-      },
-      {
-        chord: "G#m7",
-        p: "way we walk i hear the"
-      },
-      {
-        chord: "E",
-        p: "voice of you"
-      },
-      {
-        chord: "B",
-        p: "and the only thing that"
-      },
-      {
-        chord: "F#",
-        p: "i can say...the"
-      },
-      {
-        chord: "G#m",
-        p: "way is clear brightening"
-      },
-      {
-        chord: "E",
-        p: "this tragic but beautiful"
-      },
-      {
-        chord: "B",
-        p: "world without any reason our"
-      },
-      {
-        chord: "F#",
-        p: "light are shining everywhere"
-      },
-      {
-        chord: "G#m",
-        p: "now..."
-      },
-      {
-        chord: "E",
-        p: "..."
-      },
-      {
-        chord: "B",
-        p: "..."
-      },
-      {
-        chord: "F#",
-        p: "...cause we"
-      },
-      {
-        chord: "G#m",
-        p: "are"
-      },
-      {
-        chord: "E",
-        p: "...who we"
-      },
-      {
-        chord: "B",
-        p: "are..."
-      },
-      {
-        chord: "F#",
-        p: "dont let me stop this prayes"
-      },
-      {
-        chord: "G#m7",
-        p: "..."
-      },
-      {
-        chord: "E",
-        p: "..."
-      },
-    ]
+    key: "A",
+    notes: ["A3","C#4","E4"]
   },
+  {
+    key: "Abm",
+    notes: ["G#3","B3","D#4"]
+  },
+  {
+    key: "Bbm",
+    notes: ["A#3","C#4","F4"]
+  },
+  {
+    key: "B",
+    notes: ["B3","D#4","F#4"]
+  },
+  {
+    key: "Bbm7",
+    notes: ["A#3","C#4","F4","G#4"]
+  },
+  {
+    key: "Cm",
+    notes: ["C4","D#4","G4"]
+  },
+  {
+    key: "C#",
+    notes: ["C#4","F4","G#4"]
+  },
+  {
+    key: "C#m",
+    notes: ["C#4","E4","G#4"]
+  },
+  {
+    key: "C#m7",
+    notes: ["C#4","E4","G#4","B4"]
+  },
+  {
+    key: "Db",
+    notes: ["C#4","F4","G#4"]
+  },
+  {
+    key: "D",
+    notes: ["D4","F#4","A4"]
+  },
+  {
+    key: "D#",
+    notes: ["D#4","G4","A#4"]
+  },
+  {
+    key: "D#m",
+    notes: ["D#4","F#4","A#4"]
+  },
+  {
+    key: "Eb",
+    notes: ["D#4","G4","A#4"]
+  },
+  {
+    key: "Eb7",
+    notes: ["D#4","G4","A#4","C#5"]
+  },
+  {
+    key: "E",
+    notes: ["E4","G#4","B4"]
+  },
+  {
+    key: "Fm",
+    notes: ["F4","G#4","C5"]
+  },
+  {
+    key: "F#m",
+    notes: ["F#4","A4","C#5"]
+  },
+  {
+    key: "F#",
+    notes: ["F#4","A#4","C#5"]
+  },
+  {
+    key: "F",
+    notes: ["F4","A4","C5"]
+  },
+  {
+    key: "Fm7",
+    notes: ["F4","G#4","C5","D#5"]
+  },
+  {
+    key: "Gb",
+    notes: ["F#3","A#3","C#4"]
+  },
+  {
+    key: "G#",
+    notes: ["G#3","C4","D#4"]
+  },
+  {
+    key: "G#m",
+    notes: ["G#3","B3","D#4"]
+  },
+  {
+    key: "G#m7",
+    notes: ["G#3","B3","D#4","F#4"]
+  },
+  {
+    key: "Gbm7",
+    notes: ["F#3","A3","C#4","E4"]
+  }
 ]
-
 
 
 const video = document.querySelector("#video")
 const form = document.querySelector('#form');
 const chordInput = document.querySelector('#chord-input');
+let currentChord = null
 
 video.classList.add("hidden")
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+function chordIsExist(chord) {
+  return chords.some(item => item.key === chord)
+}
+
+function playBass() {
+  if (!currentChord) return
+
+  const note = currentChord[0] + "3"
+
+  const index = notes.indexOf(note) + 28
+
+  var audio = new Audio(`assets/${index}.mp3`);
+  audio.play()
+}
+
+function play() {
   let chord = chordInput.value
+  
   if (chord) chord = chord.charAt(0).toUpperCase() + chord.slice(1)
   
-  if (!chord || !audios.includes(chord)) {
-    if (!sheetIsOpen) return
+  if (!chord || !chordIsExist(chord)) {
+    if (!sheetIsOpen) {
+      if (!chord && currentChord) {
+        chord = currentChord
+      } else {
+        form.reset()
+        return
+      }
+    }
     else chord = songs[selectedSongIndex].waves[wave].chord
   }
   
   
+  const chordNotes = chords.find(item => item.key === chord).notes
+
+  for (let note of chordNotes) {
+
+    const index = notes.indexOf(note) + 28
+
+    var audio = new Audio(`assets/${index}.mp3`);
+    audio.play()
+  }
+
+
+
   chord = chord.replace(/\//g, "-");
   if (chord.includes("#")) chord = encodeURIComponent(chord)
+  
+  currentChord = chord
+  
   // console.log(chord)
-  var audio = new Audio(`assets/${chord}.mp3`);
-  audio.play()
+  // var audio = new Audio(`assets/${chord}.mp3`);
+  // audio.play()
   
   form.reset()
 
@@ -834,7 +179,31 @@ form.addEventListener('submit', (e) => {
     wave++
     changeSheet(selectedSongIndex)
   }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  play()
 });
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode === 32) {
+    event.preventDefault(); // Prevent the default spacebar behavior (e.g., scrolling down the page)
+    playBass()
+  }
+
+  if (event.keyCode === 186 || event.which === 186) {
+    event.preventDefault();
+    playBass()
+    play()
+  }
+
+  if (event.key === "3") {
+    event.preventDefault()
+    chordInput.value += "#"
+  }
+});
+
 
 
 let sheetIsOpen = false
