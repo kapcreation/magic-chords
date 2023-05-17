8b8de14fbec373dff5fb67744befe8244e824fc9
kap <kgdee7603@gmail.com>
2023-05-17 18:47:47 +0800

    commit

--- script.js
+++ script.js
@@ -111,10 +111,23 @@ const chords = [
   }
 ]
 
+const audios = []
 
+for (let i = 0; i < notes.length; i++) {
+  var audio = new Audio(`assets/${i + 28}.mp3`)
+  audios.push(audio)
+}
+
+const tip = document.querySelector("#tip")
 const video = document.querySelector("#video")
-const form = document.querySelector('#form');
-const chordInput = document.querySelector('#chord-input');
+const form = document.querySelector('#form')
+const chordInput = document.querySelector('#chord-input')
+const volumeSlider = document.querySelector("#volume-slider");
+let audioVolume = 0.5
+
+//tip
+tip.innerText = `Songs: https://chordify.net/ \n Available Chords: ${chords.map(obj => obj.key).join(", ")} \n Keys: Space = Bass | ; = Play | y = kick | h = snare | n = hihat`
+
 let currentChord = null
 
 video.classList.add("hidden")
@@ -123,21 +136,26 @@ function chordIsExist(chord) {
   return chords.some(item => item.key === chord)
 }
 
-function playBass() {
-  if (!currentChord) return
-
-  const note = currentChord[0] + "3"
-
-  const index = notes.indexOf(note) + 28
-
-  var audio = new Audio(`assets/${index}.mp3`);
+function playAudio(index) {
+  var audio = audios[index]
+  audios[index] = new Audio(`assets/${index + 28}.mp3`)
+  audio.volume = volumeSlider.value / 100
   audio.play()
+}
+
+function playBass(chord) {
+  if (!chord) return
+
+  const note = chord[0].toUpperCase() + "3"
+  
+  const index = notes.indexOf(note)
+  playAudio(index)
 }
 
 function play() {
   let chord = chordInput.value
 
-  if (chord) playBass()
+  if (chord) playBass(chord)
   
   if (chord) chord = chord.charAt(0).toUpperCase() + chord.slice(1)
   
@@ -158,10 +176,9 @@ function play() {
 
   for (let note of chordNotes) {
 
-    const index = notes.indexOf(note) + 28
+    const index = notes.indexOf(note)
 
-    var audio = new Audio(`assets/${index}.mp3`);
-    audio.play()
+    playAudio(index)
   }
 
 
@@ -189,20 +206,61 @@ form.addEventListener('submit', (e) => {
 });
 
 document.addEventListener('keydown', function(event) {
+  // press space to play bass
   if (event.keyCode === 32) {
     event.preventDefault(); // Prevent the default spacebar behavior (e.g., scrolling down the page)
-    playBass()
+    playBass(chordInput.value || currentChord)
   }
 
+  // press ; top play chord
   if (event.keyCode === 186 || event.which === 186) {
     event.preventDefault();
     play()
   }
 
+  // press 3 to change to #
   if (event.key === "3") {
     event.preventDefault()
     chordInput.value += "#"
   }
+
+  // press left arrow to decrease vol
+  if (event.key === "ArrowDown") {
+    event.preventDefault()
+    if (volumeSlider.value < 10) volumeSlider.value = 10
+    volumeSlider.value = parseInt(volumeSlider.value) - 10
+  }
+  // press right arrow to increase vol
+  if (event.key === "ArrowUp") {
+    event.preventDefault()
+    const vol = volumeSlider.value
+    if (volumeSlider.value > 90) volumeSlider.value = 90
+    volumeSlider.value = parseInt(volumeSlider.value) + 10
+  }
+
+  
+  // press y to play "kick" drum
+  if (event.key === "y") {
+    event.preventDefault()
+    const audio = new Audio("assets/drum/kick.wav")
+    audio.volume = volumeSlider.value / 100
+    audio.play()
+  }
+  // press h to play "snare" drum
+  if (event.key === "h") {
+    event.preventDefault()
+    const audio = new Audio("assets/drum/snare.wav")
+    audio.volume = volumeSlider.value / 100
+    audio.play()
+  }
+  // press n to play "hihat" drum
+  if (event.key === "n") {
+    event.preventDefault()
+    const audio = new Audio("assets/drum/hihat.wav")
+    audio.volume = volumeSlider.value / 100
+    audio.play()
+  }
+
 });
 
 
